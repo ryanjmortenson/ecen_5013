ce7410d1cbdb797ae0c25a8f35d63a0b84a1fbd6
rmortens <ryan.j.mortenson@gmail.com>
2017-09-02 16:00:39 -0600

    Working updated circbuf with opaque data

--- project/app/src/main.c
+++ project/app/src/main.c
@@ -1,12 +1,38 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include "circbuf.h"
+#include "log.h"
 
-extern int __SYSMEM_SIZE;
+#define NUM_STRUCTS (20)
+typedef struct test_struct {
+  uint16_t test1;
+  uint16_t test2;
+  uint16_t test3;
+} test_struct_t;
+
 int main()
 {
-  __SYSMEM_SIZE = 4096;
-  char * test2 = (char *)malloc(2048);
-  uint8_t test = 1;
-  test = test;
+  log_init();
+  circbuf_t * test;
+  circbuf_init(&test, NUM_STRUCTS);
+
+  for (uint32_t i = 0; i < NUM_STRUCTS; i++)
+  {
+    LOG_FATAL("MALLOCING a test_struct");
+    test_struct_t * data = malloc(sizeof(*data));
+    data->test1 = random();
+    data->test2 = random();
+    data->test3 = random();
+    LOG_HIGH("%d, %d, %d", data->test1, data->test2, data->test3);
+    circbuf_add_item(test, data);
+  }
+  for (uint32_t i = 0; i < NUM_STRUCTS; i++)
+  {
+    test_struct_t * data;
+    circbuf_remove_item(test, (void **)&data);
+    LOG_HIGH("%p, %d, %d, %d", &data, data->test1, data->test2, data->test3);
+  }
+  circbuf_destroy(test);
+  log_destroy();
   return 0;
 }
--- project/sources.mk
+++ project/sources.mk
@@ -8,11 +8,19 @@ APP_OUT=app/out
 X86_APP_OUT=$(APP_OUT)/$(X86)
 ARM_APP_OUT=$(APP_OUT)/$(ARM)
 
-APP_SRC_C += \
+NON_MAIN_SRC += \
 	$(APP_SRC_DIR)/log.c \
-	$(APP_SRC_DIR)/main.c \
 	$(APP_SRC_DIR)/profiler.c \
 	$(APP_SRC_DIR)/circbuf.c
+
+APP_SRC += \
+  $(NON_MAIN_SRC) \
+	$(APP_SRC_DIR)/main.c
+
+TEST_SRC+= \
+	$(NON_MAIN_SRC) \
+	$(APP_SRC_DIR)/unit_tests.c \
+	$(APP_SRC_DIR)/unit_circbuf.c
 
 # Make a src list without any directories to feed into the allasm/alli targets
 SRC_LIST = $(subst $(APP_SRC_DIR)/,,$(APP_SRC_C))
@@ -21,6 +29,8 @@ SRC_LIST = $(subst $(APP_SRC_DIR)/,,$(APP_SRC_CPP))
 # Build a list objects for each platform
 X86_OBJS = $(subst src,out/$(X86),$(patsubst %.c,%.o,$(APP_SRC_C)))
 ARM_OBJS = $(subst src,out/$(ARM),$(patsubst %.c,%.o,$(APP_SRC_C)))
+X86_TEST_OBJS = $(subst src,out/$(X86),$(patsubst %.c,%.o,$(TEST_SRC)))
+ARM_TEST_OBJS = $(subst src,out/$(ARM),$(patsubst %.c,%.o,$(TEST_SRC)))
 
 # Build a list of .d files to clean
 APP_DEPS += $(patsubst %.o,%.d, $(OBJS) $(25Z_OBJS) $(ARM_OBJS))
@@ -28,10 +38,7 @@ APP_DEPS += $(patsubst %.o,%.d, $(OBJS) $(25Z_OBJS) $(ARM_OBJS))
 # Add to List of items that need to be cleaned
 CLEAN+=$(ARM_OBJS) \
        $(X86_OBJS) \
-       $(CLIENT_X86_OBJS) \
-       $(CLIENT_ARM_OBJS) \
-       $(SERVER_X86_OBJS) \
-       $(SERVER_ARM_OBJS) \
+       $(X86_TEST_OBJS) \
+       $(ARM_TEST_OBJS) \
        $(APP_DEPS) \
-       $(TEST_OBJS) \
        $(APP_OUT)
