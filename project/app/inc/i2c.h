6de2b04cc7ae14fda259454488b8114093e0d43a
rmortens <ryan.j.mortenson@gmail.com>
2017-10-29 20:20:38 -0600

    Feature Commit: Remove reg from i2c functions

--- project/app/inc/i2c.h
+++ project/app/inc/i2c.h
@@ -12,42 +12,55 @@
 #include <stdint.h>
 #include "project_defs.h"
 
+typedef struct i2c_descriptor i2c_descriptor_t;
+
+/*!
+* @brief Initialize i2c bus, should be in proprietary folder
+* @param i2c_bus bus number to use for i2c
+* @param i2cd i2c descriptor
+* @return status of initialization
+*/
+status_t i2c_init(int32_t i2c_bus, i2c_descriptor_t ** i2cd, uint8_t addr);
+
+/*!
+* @brief Initialize i2c bus, should be in proprietary folder
+* @param i2c_bus bus number to use for i2c
+* @param i2cd i2c descriptor
+* @return status of initialization
+*/
+status_t i2c_dest(i2c_descriptor_t * i2cd);
 /*!
 * @brief Writes a single byte
 * @param addr address of i2c device
-* @param reg register to write byte to
 * @param byte byte to write
 * @return status of write
 */
-status_t i2c_write_byte(uint8_t addr, uint8_t reg, uint8_t byte);
+status_t i2c_write_byte(i2c_descriptor_t * i2cd, uint8_t byte);
 
 /*!
 * @brief Writes multiple bytes
 * @param addr address of i2c device
-* @param reg register to start writing bytes to
 * @param byte pointer to buffer of bytes to write
 * @param len number of bytes
 * @return status of write
 */
-status_t i2c_write_bytes(uint8_t addr, uint8_t reg, uint8_t * bytes, uint8_t len);
+status_t i2c_write_bytes(i2c_descriptor_t * i2cd, uint8_t * bytes, uint8_t len);
 
 /*!
 * @brief Reads a single byte
 * @param addr address of i2c device
-* @param reg register to read by from
 * @param byte pointer to location to store byte
 * @return status of read
 */
-status_t i2c_read_byte(uint8_t addr, uint8_t reg, uint8_t * byte);
+status_t i2c_read_byte(i2c_descriptor_t * i2cd, uint8_t * byte);
 
 /*!
 * @brief Reads multiple bytes
 * @param addr address of i2c device
-* @param reg register to start reading bytes from
 * @param bytes pointer to location to store bytes
 * @param len number of bytes to read
 * @return status of read
 */
-status_t i2c_read_bytes(uint8_t addr, uint8_t reg, uint8_t * bytes, uint8_t len);
+status_t i2c_read_bytes(i2c_descriptor_t * i2cd, uint8_t * bytes, uint8_t len);
 
 #endif /* _I2C_H */
--- project/prop/bbb/src/i2c.c
+++ project/prop/bbb/src/i2c.c
@@ -6,40 +6,129 @@
 *
 */
 
+#include <fcntl.h>
+#include <errno.h>
+#include <linux/i2c-dev.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "i2c.h"
 #include "log.h"
 #include "project_defs.h"
 
-status_t i2c_write_byte(uint8_t addr, uint8_t reg, uint8_t byte)
-{
-  FUNC_ENTRY;
-  return SUCCESS;
-}
+#define I2C_PATH "/dev/i2c-"
+#define I2C_PATH_MAX_LEN (256)
 
-status_t i2c_write_bytes(uint8_t addr, uint8_t reg, uint8_t * bytes, uint8_t len)
-{
-  FUNC_ENTRY;
-  CHECK_NULL(bytes);
-  return SUCCESS;
-}
+struct i2c_descriptor {
+  int32_t fd;
+  int32_t addr;
+};
 
-status_t i2c_read_byte(uint8_t addr, uint8_t reg, uint8_t * byte)
+status_t i2c_init(int32_t i2c_bus, i2c_descriptor_t ** i2cd, uint8_t addr)
 {
   FUNC_ENTRY;
-  CHECK_NULL(byte);
-  *byte = 12;
-  return SUCCESS;
-}
+  status_t status = SUCCESS;
+  char i2c_path[I2C_PATH_MAX_LEN] = {0};
+  int32_t res;
+  int32_t fd;
 
-status_t i2c_read_bytes(uint8_t addr, uint8_t reg, uint8_t * bytes, uint8_t len)
-{
-  FUNC_ENTRY;
-  CHECK_NULL(bytes);
-  for (uint8_t i = 0; i < len; i++)
+  do
   {
-    *(bytes + i) = 15;
+    // Create path filename
+    memcpy(i2c_path, I2C_PATH, strlen(I2C_PATH));
+    snprintf(i2c_path, I2C_PATH_MAX_LEN, "%s%d", I2C_PATH, i2c_bus);
+    *i2cd = malloc(sizeof(**i2cd));
+    if (*i2cd == NULL)
+    {
+      LOG_ERROR("Could not malloc a new i2c descriptor");
+      status = FAILURE;
+      break;
+    }
+
+    LOG_HIGH("Opening I2C device %s", i2c_path);
+    fd = open(i2c_path, O_RDWR);
+    if (fd < 0)
+    {
+      LOG_ERROR("Could not open %s, %s", i2c_path, strerror(errno));
+      status = FAILURE;
+      break;
+    }
+
+    LOG_MED("Initializing communication with address 0x%02x", addr);
+    res = ioctl(fd, I2C_SLAVE, addr);
+    if (res < 0)
+    {
+      LOG_ERROR("Could not open %s, %s", i2c_path, strerror(errno));
+      status = FAILURE;
+      break;
+    }
+  } while(0);
+
+  if (status == SUCCESS)
+  {
+    LOG_FATAL("fd: %d", fd);
+    (*i2cd)->fd = fd;
+    (*i2cd)->addr = addr;
   }
+  return status;
+}
+
+status_t i2c_dest(i2c_descriptor_t * i2cd)
+{
+  free(i2cd);
+  close(i2cd->fd);
   return SUCCESS;
 }
+
+status_t i2c_write_bytes(i2c_descriptor_t * i2cd, uint8_t * bytes, uint8_t len)
+{
+  FUNC_ENTRY;
+  CHECK_NULL(bytes);
+  int32_t res;
+  status_t status = SUCCESS;
+
+  res = write(i2cd->fd, bytes, len);
+  if (res != len)
+  {
+    LOG_ERROR("Could not write %d bytes to addr: %d, %s",
+              len,
+              i2cd->addr,
+              strerror(errno));
+    status = FAILURE;
+  }
+  return status;
+}
+
+status_t i2c_write_byte(i2c_descriptor_t * i2cd, uint8_t byte)
+{
+  FUNC_ENTRY;
+  return i2c_write_bytes(i2cd, &byte, 1);
+}
+
+status_t i2c_read_bytes(i2c_descriptor_t * i2cd, uint8_t * bytes, uint8_t len)
+{
+  FUNC_ENTRY;
+  CHECK_NULL(bytes);
+  int32_t res;
+  status_t status = SUCCESS;
+
+  res = read(i2cd->fd, bytes, len);
+  if (res != len)
+  {
+    LOG_ERROR("Could not read %d byte from addr: %d", len, i2cd->addr);
+    status = FAILURE;
+  }
+  return status;
+}
+
+status_t i2c_read_byte(i2c_descriptor_t * i2cd, uint8_t * byte)
+{
+  FUNC_ENTRY;
+  return i2c_read_bytes(i2cd, byte, 1);
+}
+
