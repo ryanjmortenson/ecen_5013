34a4172ddfe1eb16f44c2d83c268926c0527a78a
rmortens <ryan.j.mortenson@gmail.com>
2017-11-18 13:42:29 -0700

    Design stubs for open, release, read, write

--- homework/hw4/prob1/led_module.c
+++ homework/hw4/prob1/led_module.c
@@ -3,26 +3,39 @@
 #include <linux/fs.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/slab.h>
 #include <linux/timer.h>
 #include <linux/uaccess.h>
 
 #define NUM_CHAR_DRIVERS (1)
+
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Ryan Mortenson");
 MODULE_DESCRIPTION("LED Control module");
 MODULE_VERSION("0");
 
-static ssize_t led_read(struct file * fp, char __user * buffer, size_t len, loff_t * offset);
 static int led_open(struct inode * node, struct file * fp);
-dev_t led_device;
+static int led_release(struct inode * node, struct file * fp);
+static ssize_t led_read(struct file * fp, char __user * buffer, size_t len, loff_t * offset);
+static ssize_t led_write(struct file * fp, const char __user * buffer, size_t len, loff_t * offset);
+
+struct private_data {
+  unsigned int period;
+  unsigned int duty_cycle;
+  unsigned int state;
+  unsigned int read_state;
+};
 
 struct file_operations led_fops = {
   .owner    = THIS_MODULE,
   .open     = led_open,
-  .read     = led_read
+  .read     = led_read,
+  .write    = led_write,
+  .release  = led_release
 };
 
+dev_t led_device;
 struct cdev led_cdev;
 
 static int __init led_module_init(void)
@@ -64,7 +77,23 @@ module_exit(led_module_exit);
 
 static int led_open(struct inode * node, struct file * fp)
 {
+  char * buf;
+  struct private_data * p_data;
+
   printk(KERN_DEBUG "open()");
+
+  buf = kmalloc(sizeof(struct private_data), GFP_KERNEL);
+  if (buf == NULL)
+  {
+    printk(KERN_ERR "Could not malloc memory for private data");
+    return -1;
+  }
+  p_data = (struct private_data *) buf;
+  p_data->period = 0;
+  p_data->duty_cycle = 0;
+  p_data->state = 0;
+  p_data->read_state = 0;
+  fp->private_data = p_data;
   return 0;
 }
 
@@ -72,8 +101,48 @@ static ssize_t led_read(struct file * fp, char __user * buffer, size_t len, loff
 {
   int res = 0;
   printk(KERN_DEBUG "read()");
-  res = copy_to_user(buffer, "HELLO\n", 7);
-  printk(KERN_INFO "Copy to user res %d", res);
-  printk(KERN_INFO "length %d", len);
-  return 6;
+  if (!((struct private_data *)fp->private_data)->read_state)
+  {
+    res = copy_to_user(buffer, "HELLO\n", 7);
+    printk(KERN_INFO "Copy to user res %d", res);
+    printk(KERN_INFO "length %d", len);
+    ((struct private_data *)fp->private_data)->read_state = 1;
+    return 6;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+static ssize_t led_write(struct file * fp, const char __user * buffer, size_t len, loff_t * offset)
+{
+  char * buf;
+  s32 res;
+
+  printk(KERN_DEBUG "write()");
+  printk(KERN_DEBUG "len %d", len);
+  buf = kmalloc(len, GFP_KERNEL);
+  if (buf == NULL)
+  {
+    printk(KERN_ERR "Could not malloc memory for user to kernel space copy");
+    return 0;
+  }
+
+  res = copy_from_user(buf, buffer, len);
+  if (res != 0)
+  {
+    printk(KERN_ERR "Could not copy from user space");
+    return 0;
+  }
+
+  printk(KERN_DEBUG "%s", buf);
+  return len;
+}
+
+static int led_release(struct inode * node, struct file * fp)
+{
+  printk(KERN_DEBUG "release()");
+  kfree(fp->private_data);
+  return 0;
 }
