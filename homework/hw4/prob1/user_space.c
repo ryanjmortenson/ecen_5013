a399b74eb6fead7da19e8d7a6e4f158e4c489281
rmortens <ryan.j.mortenson@gmail.com>
2017-11-19 10:01:47 -0700

    Comment user space driver for module

--- homework/hw4/prob1/user_space.c
+++ homework/hw4/prob1/user_space.c
@@ -12,6 +12,7 @@
 #define CMD_SIZE (7)
 #define BUF_SIZE (256)
 
+// Command structure used to send data to module
 typedef struct cmd {
   uint32_t period_ms;
   uint8_t duty_cycle;
@@ -19,6 +20,7 @@ typedef struct cmd {
   uint8_t blink;
 } cmd_t;
 
+// Different types of reads
 typedef enum read_cmd {
   READ_ALL,
   READ_PERIOD,
@@ -27,6 +29,7 @@ typedef enum read_cmd {
   READ_BLINK_STATE
 } read_cmd_t;
 
+// An initial command that will be changed throughout testing
 cmd_t cmd0 = {
   .period_ms = 500,
   .duty_cycle = 25,
@@ -34,6 +37,10 @@ cmd_t cmd0 = {
   .blink = 0
 };
 
+/*!
+* @brief print a filled out command structure
+* @param command pointer to command structure to print
+*/
 static inline void print_cmd(cmd_t * command)
 {
   printf("Writing a command with the following information\n");
@@ -43,12 +50,22 @@ static inline void print_cmd(cmd_t * command)
   printf("Blink %d\n\n", command->blink);
 }
 
+/*!
+* @brief Print a response from the kernel module
+* @param buffer holding response
+*/
 static inline void print_rsp(char * buffer)
 {
   printf("Response from module\n");
   printf("%s\n", buffer);
 }
 
+/*!
+* @brief Copy a command structure into a buffer
+* @param command to copy
+* @param buffer to copy command into
+* @return status of copy
+*/
 static inline int copy_cmd(cmd_t * command, char * buf)
 {
   if (buf == NULL)
@@ -67,6 +84,11 @@ static inline int copy_cmd(cmd_t * command, char * buf)
   return 0;
 }
 
+/*!
+* @brief Run a command and get current status of led module
+* @param command to run
+* @return status of running command
+*/
 static inline int32_t run_cmd(int fd, cmd_t * cmd)
 {
   char buffer[BUF_SIZE] = {0};
@@ -97,6 +119,10 @@ static inline int32_t run_cmd(int fd, cmd_t * cmd)
   print_rsp(buffer);
 }
 
+/*!
+* @brief Run a bunch of commands to exercise led module
+* @return status of executing commands
+*/
 int main()
 {
   int fd = 0;
@@ -110,6 +136,8 @@ int main()
     return 0;
   }
 
+  // Write poorly formatted command to led module which should handle it
+  // gracefully
   printf("Writing nothing to driver, it should fail gracefully\n");
   res = write(fd, buffer, 0);
   if (res > 0)
@@ -118,17 +146,20 @@ int main()
     return -1;
   }
 
+  // Run a correctly formatted command
   if (run_cmd(fd, &cmd0) == -1)
   {
     return -1;
   }
 
+  // Run a correctly formatted command with blink on
   cmd0.blink = 1;
   if (run_cmd(fd, &cmd0) == -1)
   {
     return -1;
   }
 
+  // Run a correctly formatted command with blink off and read blink state
   cmd0.blink = 0;
   cmd0.read_command = READ_BLINK_STATE;
   if (run_cmd(fd, &cmd0) == -1)
@@ -136,18 +167,21 @@ int main()
     return -1;
   }
 
+  // Run a correctly formatted and read period
   cmd0.read_command = READ_PERIOD;
   if (run_cmd(fd, &cmd0) == -1)
   {
     return -1;
   }
 
+  // Run a correctly formatted and read duty cycle
   cmd0.read_command = READ_DUTY_CYCLE;
   if (run_cmd(fd, &cmd0) == -1)
   {
     return -1;
   }
 
+  // Run a correctly formatted and read led state
   cmd0.blink = 1;
   cmd0.period_ms = 1000;
   cmd0.duty_cycle = 50;
@@ -157,12 +191,14 @@ int main()
     return -1;
   }
 
+  // Run a correctly formatted and all
   cmd0.read_command = READ_ALL;
   if (run_cmd(fd, &cmd0) == -1)
   {
     return -1;
   }
 
+  // Close file descriptor
   close(fd);
   return 0;
 }
