3c14a79ba4a89b38ebd5dd725fa4b5ee8bae2b7d
rmortens <ryan.j.mortenson@gmail.com>
2017-11-25 12:14:09 -0700

    Feature Commit: Add Tiva ware driverlib
    - Add blinky light status for "main task" on tiva

--- project/app/src/tiva_main.c
+++ project/app/src/tiva_main.c
@@ -1,6 +1,17 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+// Free RTOS includes
 #include "FreeRTOS.h"
-#include "string.h"
 #include "task.h"
+
+// TIVA Ware include
+#include "inc/hw_memmap.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+
+// Project includes
 #include "light.h"
 #include "workers.h"
 #include "pthread_wrapper.h"
@@ -9,6 +20,18 @@
 #include "temp.h"
 
 int32_t abort_signal;
+
+/*!
+* @brief Prep the onboard led
+* @param[in] task_handle handle to the task
+* @param[in] task_name tasks name
+*/
+void prep_led()
+{
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION));
+  GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+}
 
 /*!
 * @brief Statck overflow function required by freertos
@@ -37,12 +60,17 @@ static void * cb (void * params)
 */
 static void task(void *params)
 {
+  uint8_t led_state = 0;
+
   message_t msg = MSG_INIT(LOG, MAIN_TASK, MAIN_TASK);
   mqd_t msg_q = get_writeable_queue();
   for(;;)
   {
+    led_state = ~led_state;
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, led_state);
     send_msg(msg_q, &msg, NULL, 0);
     usleep(100);
+
   }
   PTHREAD_RETURN(NULL);
 }
@@ -55,6 +83,8 @@ static void task(void *params)
 int main() {
   pthread_t test;
   int32_t res;
+
+  prep_led();
 
   res = pthread_create(&test, NULL, task, NULL);
   if (res != 0)
