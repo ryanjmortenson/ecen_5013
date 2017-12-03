05d3055762284e8e9b16e6a978eeaa1fe6dd2def
rmortens <ryan.j.mortenson@gmail.com>
2017-12-03 05:35:52 -0700

    Feature Commit: Up workers server_test.c

--- project/app/src/server_test.c
+++ project/app/src/server_test.c
@@ -6,19 +6,12 @@
 #include "server_task.h"
 #include "workers.h"
 
-void * cb(void * param)
-{
-  LOG_FATAL("Received a message");
-  return NULL;
-}
-
 int main()
 {
   FUNC_ENTRY;
-  init_workers(1);
+  init_workers(5);
   server_init();
   log_msg_init("server_test.log", 1);
-  register_cb(LOG, LOG_TASK, cb);
   while(1)
   {
     usleep(1000000);
