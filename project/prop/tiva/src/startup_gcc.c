0eba43dfc9b350b19b2a4d43a31e8f9abe1934f0
Ryan Mortenson <ryan.j.mortenson@gmail.com>
2017-12-06 21:12:22 -0700

    Code Cleanup: Remove unused files

--- project/prop/tiva/src/startup_gcc.c
+++ project/prop/tiva/src/startup_gcc.c
@@ -1,26 +1,10 @@
-//*****************************************************************************
-//
-// startup_gcc.c - Startup code for use with GNU tools.
-//
-// Copyright (c) 2013-2017 Texas Instruments Incorporated.  All rights reserved.
-// Software License Agreement
-//
-// Texas Instruments (TI) is supplying this software for use solely and
-// exclusively on TI's microcontroller products. The software is owned by
-// TI and/or its suppliers, and is protected under applicable copyright
-// laws. You may not combine this software with "viral" open-source
-// software in order to form a larger program.
-//
-// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
-// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
-// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
-// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
-// DAMAGES, FOR ANY REASON WHATSOEVER.
-//
-// This is part of revision 2.1.4.178 of the EK-TM4C1294XL Firmware Package.
-//
-//*****************************************************************************
+/** @file startup_gcc.c
+*
+* @brief Start up for tiva, adapted from startup_gcc.c from blinky example
+* @author Ryan Mortenson
+* @tools GCC 5.4.0, vim 7.4, make 4.1, Ubuntu 16.04
+*
+*/
 
 #include <stdint.h>
 #include "hw_nvic.h"
